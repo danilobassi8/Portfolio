c8cff5fe0e24819df754efa4fe40963afc93f9d8
CI <CI@example.com>
2021-06-24 03:26:18 +0000

    deploy

---
+++ precache-manifest.4cf0faf315a777fcb4c126eeeafff2a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4f84d9bc36e7ddd0d3",
+    "url": "/Portfolio/css/app.fe74831e.css"
+  },
+  {
+    "revision": "95e4f7cbe43b7afa042b",
+    "url": "/Portfolio/css/chunk-vendors.5d27a9d6.css"
+  },
+  {
+    "revision": "64031b8366a1853d264c88b7b3fff0a0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2f4f84d9bc36e7ddd0d3",
+    "url": "/Portfolio/js/app.2d6f1280.js"
+  },
+  {
+    "revision": "95e4f7cbe43b7afa042b",
+    "url": "/Portfolio/js/chunk-vendors.77ed2fa1.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/Portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Portfolio/robots.txt"
+  }
+]);
